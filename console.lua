82d5d6218f86ff8a892a52e5d7eb3b50cd1b9b45
Julian Montague <roast@thatoneprogrammerkid.com>
2016-02-24 00:21:43 -0600

    Add console:print()
    This commit will add the console:print() function to write text to the console while also handling text wrapping.

--- console.lua
+++ console.lua
@@ -18,6 +18,13 @@ function console:init( xPos, yPos, xSize, ySize, font, bgColor )
   console.info.font = font or love.graphics.newFont(12)
 end
 
+function console:print( text )
+  local width, wrapped = console.info.font:getWrap( text, console.info.xSize - 20 ) -- wrap the text, using the console width - 20 to take the padding into account
+  for k, line in ipairs( wrapped ) do
+    table.insert( console.lines, line )
+  end
+end
+
 function console:draw()
   if not console.initiated then return end
   
